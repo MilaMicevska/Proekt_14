7a246118b0778aad06064c627a33e193b8580719
MilaMicevska <micevska.mila@gmail.com>
2023-12-24 10:34:30 +0100

    p14

---
+++ src/proekt14/varijabili.java
@@ -0,0 +1,20 @@
+package proekt14;
+
+public class varijabili {
+	public static void main (String[] args) {
+		double x,z,y;
+		if (x>y||z>y) {
+			System.out.println("1");
+		}else {
+			System.out.println("2");
+		}
+		if (Math.abs(x-y)>=z) {
+			if (x>y)
+				System.out.println("3");
+		}else {
+				System.out.println("4");
+		}else {
+			System.out.println("5");
+		}
+	}
+}
